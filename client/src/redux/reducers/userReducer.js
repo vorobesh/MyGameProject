0465d7c7188a1e27d57174fcfd1548dfb311b05d
Andrey <surgutandrey96@gmail.com>
2022-04-21 19:36:50 +0300

    NavAndReducers

---
+++ client/src/redux/reducers/userReducer.js
@@ -0,0 +1,25 @@
+const localStorageUserStr = localStorage.getItem('user');
+
+const localStorageUser = localStorageUserStr ?
+  JSON.parse(localStorageUserStr).userCreatedOrLoggining :
+  undefined;
+
+const initialState = { user: localStorageUser, infoFromBack: '' }
+
+export function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'REGISTER_OR_LOGIN_USER':
+      return {...state, user: action.payload.userCreatedOrLoggining,
+        infoFromBack: action.payload.message}
+
+        case 'CLEAR_MESSAGE':
+          return {...state, infoFromBack: ''}
+    
+        case 'LOGOUT_USER':
+          return {...state, user: undefined}
+          
+    default:
+      return state
+  }
+
+}
