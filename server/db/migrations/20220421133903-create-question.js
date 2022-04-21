a91c82ad761482aa4bc13946ea60c40d8cdceeb9
Zhenya <kimzh118@gmail.com>
2022-04-21 16:56:33 +0300

    DB

---
+++ server/db/migrations/20220421133903-create-question.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      question_answer: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      topic_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Questions',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  },
+};
