361377c38e53a173fb4a3e0a0d460cd4d51c29fc
Andrey <surgutandrey96@gmail.com>
2022-04-22 14:15:32 +0300

    score

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -15,7 +15,7 @@ function Profile(props) {
     {/* <div className={style.profile}> */}
     <div className={style.profile} align='center'>
         <div className="col s12 m6" >
-          <div className="card blue-grey darken-1" >
+          <div className="card green darken-1" >
             <div className='card-content white-text'>
               <span className="card-title"> Профиль </span>
               {!edit ?
--- client/src/redux/reducers/gameBoardReducer.js
+++ client/src/redux/reducers/gameBoardReducer.js
@@ -1,9 +1,12 @@
-const initialState = { topics: [] };
+const initialState = { topics: [], points: 0};
 
 export function gameBoardReducer(state = initialState, action) {
   switch (action.type) {
     case 'INIT_TOPICS':
       return { ...state, topics: action.payload }
+    case 'EDIT_POINTS' :
+      return {...state, points: state.points + action.payload }
+
     default:
       return state
   }
